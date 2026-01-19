0e6930ed98ec0e68e883f967915544376254b13e
aurevia-1 <info.aureviacreations@gmail.com>
2026-01-19 16:47:08 +0500

    URL Set

--- src/config/approuter.tsx
+++ src/config/approuter.tsx
@@ -1,17 +1,17 @@
-import { HashRouter, Route, Routes } from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import ProjectScreen from "../pages/ProjectScreen";
 import Home from "../pages/Home";
 
 export default function AppRouter() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />      
           <Route path="/space/:spaceId/project/:projectId" element={<ProjectScreen />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
